55113ac5247fd8fcc66db62cfa39c8037b4eb4a0
Luis <luiscoar@outlook.com>
2020-04-14 23:23:23 -0500

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortadaComponent } from './pages/portada/portada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule
